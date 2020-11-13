c2f2ccd9829ebd6289cd09d62eb64e2ae6c4d174
X88Y <63887301+X88Y@users.noreply.github.com>
2020-11-13 21:32:13 +0300

    Add File

---
+++ main.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+patern = r"name=\"a\d+\" type=\"\w+\" value=..."
+url = input('Owned by Duxk: ')
+
+source_code = requests.get(url)
+html = BeautifulSoup(source_code.text, 'html.parser')
+question = html.find_all('div', {"class": "label"})
+question = str(question).split('</div>, <div class=\"label\">')
+
+for i in range(len(question)):
+    tasks = str(html).split('<div class=\"q\" id="')
+    temp = tasks[i + 1]
+    TempAnswer = re.findall(patern, temp)
+    if len(TempAnswer) > 1:
+        LastTemp = ((str(TempAnswer).split('name="')[1].split('"')[0])[1:])
+        print('Задание ' + LastTemp)
+        for answer in TempAnswer:
+            print(answer[-2].replace('1', '+').replace('0', '-'))
